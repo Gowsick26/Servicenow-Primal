59ba973e24d356ecf061515895255f3384cb0ad0
Gowsick26 <112756821+Gowsick26@users.noreply.github.com>
2023-11-06 16:49:59 +0530

    Add files via upload

---
+++ day 4/b.js
@@ -0,0 +1,5 @@
+const mainDiv = document.getElementById("mainDiv");
+const paragraphs = document.querySelectorAll(".para");
+paragraphs.forEach(paragraph => {
+    paragraph.style.color = "red";
+});
